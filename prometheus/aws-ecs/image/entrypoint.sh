14b84615e714ea68d0f93a5417237dec9e495834
giltk <76215188+giltk@users.noreply.github.com>
2022-10-04 16:36:19 +0300

    feat: initial release of prometheus node exporter

---
+++ prometheus/aws-ecs/image/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [[ -z $CORALOGIX_PRIVATEKEY ]]; then
+  echo "CORALOGIX_PRIVATEKEY environment variable must be set"
+  exit 1
+fi
+
+if [[ -z $CORALOGIX_ENDPOINT ]]; then
+  echo "CORALOGIX_ENDPOINT environment variable must be set"
+  exit 1
+fi
+
+sed -i "s/<PRIVATEKEY>/$CORALOGIX_PRIVATEKEY/" /etc/prometheus/prometheus.yml 
+sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml 
+
+/bin/prometheus $@
