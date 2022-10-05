e8641007b8de832e4465e44a1f00d7679254849c
giltk <76215188+giltk@users.noreply.github.com>
2022-10-05 22:22:05 +0300

    feat: added docker hostname as the instance label

--- prometheus/aws-ecs/image/entrypoint.sh
+++ prometheus/aws-ecs/image/entrypoint.sh
@@ -11,7 +11,10 @@ if [[ -z $CORALOGIX_ENDPOINT ]]; then
   exit 1
 fi
 
+DOCKERHOST=`cat /hostetc/hostname`
+
 sed -i "s/<PRIVATEKEY>/$CORALOGIX_PRIVATEKEY/" /etc/prometheus/prometheus.yml 
 sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml 
+sed -i "s/<HOSTNAME>/$DOCKERHOST/" /etc/prometheus/prometheus.yml
 
 /bin/prometheus $@
