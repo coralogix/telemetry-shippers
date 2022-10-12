a4bafb4786d1ddff277fdca4f55140bef6ba6e59
giltk <76215188+giltk@users.noreply.github.com>
2022-10-12 18:48:08 +0300

    feat: added cAdvisor for container metrics. - added 'scrape_interval' to environment variables - modifed the taskdefinition.json - modified the entrypoint.sh and prometheus.yml

--- prometheus/aws-ecs/image/entrypoint.sh
+++ prometheus/aws-ecs/image/entrypoint.sh
@@ -11,10 +11,20 @@ if [[ -z $CORALOGIX_ENDPOINT ]]; then
   exit 1
 fi
 
+if [[ -z $SCRAPE_INTERVAL ]]; then
+  echo "SCRAPE_INTERVAL environment variable must be set"
+  exit 1
+fi
+
 DOCKERHOST=`cat /hostetc/hostname`
+CLUSTERNAME=`wget -qO- http://172.17.0.1:51678/v1/metadata | sed -e 's/[{}]/''/g' | awk -F , '{split($1,a,"\"");print a[4]}'`
+HOSTPUBLICIP=`wget -qO- http://checkip.amazonaws.com`
 
 sed -i "s/<PRIVATEKEY>/$CORALOGIX_PRIVATEKEY/" /etc/prometheus/prometheus.yml 
-sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml 
-sed -i "s/<HOSTNAME>/$DOCKERHOST/" /etc/prometheus/prometheus.yml
+sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml
+sed -i "s/<SCRAPE-INTERVAL>/$SCRAPE_INTERVAL/g" /etc/prometheus/prometheus.yml 
+sed -i "s/<HOSTNAME>/$DOCKERHOST/g" /etc/prometheus/prometheus.yml
+sed -i "s/<CLUSTERNAME>/$CLUSTERNAME/" /etc/prometheus/prometheus.yml
+sed -i "s/<HOSTPUBLICIP>/$HOSTPUBLICIP/" /etc/prometheus/prometheus.yml
 
 /bin/prometheus $@
