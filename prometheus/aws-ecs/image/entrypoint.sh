fc8ae172f889f46c6bc98cb769ce461a91fa63ad
giltk <76215188+giltk@users.noreply.github.com>
2022-10-13 09:33:16 +0300

    feat: added cAdvisor for container metrics. (#99)
    * feat: added cAdvisor for container metrics.
    - added 'scrape_interval' to environment variables
    - modifed the taskdefinition.json
    - modified the entrypoint.sh and prometheus.yml
    
    * fix: adding grafana dashboards
    
    * fix: added variables check to entrypoint.sh

--- prometheus/aws-ecs/image/entrypoint.sh
+++ prometheus/aws-ecs/image/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+# Get docker host 
+DOCKERHOST=`cat /hostetc/hostname`
+
+# Get Cluster name from the ecs-agent
+CLUSTERNAME=`wget -qO- http://172.17.0.1:51678/v1/metadata | sed -e 's/[{}]/''/g' | awk -F , '{split($1,a,"\"");print a[4]}'`
+
+# Get host public ip
+HOSTPUBLICIP=`wget -qO- http://checkip.amazonaws.com`
+
+# Variables check
 if [[ -z $CORALOGIX_PRIVATEKEY ]]; then
   echo "CORALOGIX_PRIVATEKEY environment variable must be set"
   exit 1
@@ -11,10 +21,31 @@ if [[ -z $CORALOGIX_ENDPOINT ]]; then
   exit 1
 fi
 
-DOCKERHOST=`cat /hostetc/hostname`
+if [[ -z $SCRAPE_INTERVAL ]]; then
+  echo "SCRAPE_INTERVAL environment variable must be set"
+  exit 1
+fi
+
+if [[ -z $DOCKERHOST ]]; then
+  echo "error while retrieving instance hostname, verify that /etc/hostname is mounted"
+  exit 1
+fi
+
+if [[ -z $CLUSTERNAME ]]; then
+  echo "error while retrieving clustername from ecs-agent"
+  exit 1
+fi
+
+if [[ -z $HOSTPUBLICIP ]]; then
+  echo "error while retrieving host public ip, verify that host has outgoing ports (80,443) open"
+  exit 1
+fi
 
 sed -i "s/<PRIVATEKEY>/$CORALOGIX_PRIVATEKEY/" /etc/prometheus/prometheus.yml 
-sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml 
-sed -i "s/<HOSTNAME>/$DOCKERHOST/" /etc/prometheus/prometheus.yml
+sed -i "s|<ENDPOINT>|$CORALOGIX_ENDPOINT|" /etc/prometheus/prometheus.yml
+sed -i "s/<SCRAPE-INTERVAL>/$SCRAPE_INTERVAL/g" /etc/prometheus/prometheus.yml 
+sed -i "s/<HOSTNAME>/$DOCKERHOST/g" /etc/prometheus/prometheus.yml
+sed -i "s/<CLUSTERNAME>/$CLUSTERNAME/" /etc/prometheus/prometheus.yml
+sed -i "s/<HOSTPUBLICIP>/$HOSTPUBLICIP/" /etc/prometheus/prometheus.yml
 
 /bin/prometheus $@
