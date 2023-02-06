8cc53f9246a5255db9ac00181b573de0325f06f9
Shaun Remekie <shaun.remekie@coralogix.com>
2023-02-06 23:39:09 +0100

    updated Dockerfile

---
+++ otel-agent/ECS/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CONFIG_PATH=/config.yaml
+
+# decode base64 encoded env var and write to file
+echo ${OTEL_CONFIG} | base64 -d > ${CONFIG_PATH}
+
+echo "${CONFIG_PATH}:"
+cat ${CONFIG_PATH}
+
+# run otel agent
+exec /otelcontribcol --config ${CONFIG_PATH}
