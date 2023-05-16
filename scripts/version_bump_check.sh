820c2b9ddf6fde9dbe7a0e5cabf0ce5f4d57f3a7
Matej Gera <matej.gera@coralogix.com>
2023-05-16 17:17:13 +0200

    Formatting
    Signed-off-by: Matej Gera <matej.gera@coralogix.com>

--- scripts/version_bump_check.sh
+++ scripts/version_bump_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Simply check if diff in 'version' exists in the chart file.
 git diff $1 | grep -q "+version";
 if [ $? -ne 0 ]; then
   echo "Chart version in $1 needs to be updated."
