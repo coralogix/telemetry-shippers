9d3387aecb3cfd976e4fdf6b41a13b185139af09
Shaun Remekie <shaun.remekie@coralogix.com>
2023-07-12 13:20:47 +0200

    removed reflection and replaced it wit static mapping

--- otel-agent/ecs-ec2/ecsattributesprocessor/metadata.go
+++ otel-agent/ecs-ec2/ecsattributesprocessor/metadata.go
@@ -2,105 +2,99 @@ package ecsattributesprocessor
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
 	"time"
 )
 
 type Metadata struct {
-	ContainerARN  string    `json:"ContainerARN" flat:"aws.ecs.container.arn"`
-	CreatedAt     time.Time `json:"CreatedAt" flat:"created.at"`
-	DesiredStatus string    `json:"DesiredStatus" flat:"desired.status"`
-	DockerID      string    `json:"DockerId" flat:"docker.id"`
-	DockerName    string    `json:"DockerName" flat:"docker.name"`
-	Image         string    `json:"Image" flat:"image"`
-	ImageID       string    `json:"ImageID" flat:"image.id"`
-	KnownStatus   string    `json:"KnownStatus" flat:"aws.ecs.task.known.status"`
-	Labels        Labels    `json:"Labels" flat:""`
+	ContainerARN  string    `json:"ContainerARN"`
+	CreatedAt     time.Time `json:"CreatedAt"`
+	DesiredStatus string    `json:"DesiredStatus"`
+	DockerID      string    `json:"DockerId"`
+	DockerName    string    `json:"DockerName"`
+	Image         string    `json:"Image"`
+	ImageID       string    `json:"ImageID"`
+	KnownStatus   string    `json:"KnownStatus"`
+	Labels        Labels    `json:"Labels"`
 	Limits        struct {
-		CPU    int `json:"CPU" flat:"cpu"`
-		Memory int `json:"Memory" flat:"memory"`
-	} `json:"Limits" flat:"limits"`
-	Name      string    `json:"Name" flat:"name"`
-	Networks  []Network `json:"Networks" flat:"networks"`
-	Ports     []Port    `json:"Ports" flat:"ports"`
-	StartedAt time.Time `json:"StartedAt" flat:"started.at"`
-	Type      string    `json:"Type" flat:"type"`
-	Volumes   []Volume  `json:"Volumes" flat:"volumes"`
+		CPU    int `json:"CPU"`
+		Memory int `json:"Memory"`
+	} `json:"Limits"`
+	Name      string    `json:"Name"`
+	Networks  []Network `json:"Networks"`
+	Ports     []Port    `json:"Ports"`
+	StartedAt time.Time `json:"StartedAt"`
+	Type      string    `json:"Type"`
+	Volumes   []Volume  `json:"Volumes"`
 }
 
 type Labels struct {
-	ComAmazonawsEcsCluster               string `json:"com.amazonaws.ecs.cluster" flat:"aws.ecs.cluster"`
-	ComAmazonawsEcsContainerName         string `json:"com.amazonaws.ecs.container-name" flat:"aws.ecs.container.name"`
-	ComAmazonawsEcsTaskArn               string `json:"com.amazonaws.ecs.task-arn" flat:"aws.ecs.task.arn"`
-	ComAmazonawsEcsTaskDefinitionFamily  string `json:"com.amazonaws.ecs.task-definition-family" flat:"aws.ecs.task.definition.family"`
-	ComAmazonawsEcsTaskDefinitionVersion string `json:"com.amazonaws.ecs.task-definition-version" flat:"aws.ecs.task.definition.version"`
+	ComAmazonawsEcsCluster               string `json:"com.amazonaws.ecs.cluster"`
+	ComAmazonawsEcsContainerName         string `json:"com.amazonaws.ecs.container-name"`
+	ComAmazonawsEcsTaskArn               string `json:"com.amazonaws.ecs.task-arn"`
+	ComAmazonawsEcsTaskDefinitionFamily  string `json:"com.amazonaws.ecs.task-definition-family"`
+	ComAmazonawsEcsTaskDefinitionVersion string `json:"com.amazonaws.ecs.task-definition-version"`
 }
 
 type Network struct {
-	IPv4Addresses []string `json:"IPv4Addresses" flat:"ipv4.addresses"`
-	NetworkMode   string   `json:"NetworkMode" flat:"network.mode"`
+	IPv4Addresses []string `json:"IPv4Addresses"`
+	NetworkMode   string   `json:"NetworkMode"`
 }
 
 type Port struct {
-	ContainerPort int    `json:"ContainerPort" flat:"container.port"`
-	HostIP        string `json:"HostIp" flat:"host.ip"`
-	HostPort      int    `json:"HostPort" flat:"host.port"`
-	Protocol      string `json:"Protocol" flat:"protocol"`
+	ContainerPort int    `json:"ContainerPort"`
+	HostIP        string `json:"HostIp"`
+	HostPort      int    `json:"HostPort"`
+	Protocol      string `json:"Protocol"`
 }
 
 type Volume struct {
-	Destination string `json:"Destination" flat:"destination"`
-	Source      string `json:"Source" flat:"source"`
+	Destination string `json:"Destination"`
+	Source      string `json:"Source"`
 }
 
-// Flat - returns flat map representation of the metadata structure, using the "flat" tag
-func (m *Metadata) Flat() map[string]interface{} {
-	result := make(map[string]interface{})
-	flattenType("", reflect.ValueOf(*m), result)
-	return result
-}
+func (m *Metadata) Flat() map[string]any {
+	flattened := make(map[string]any)
 
-func flattenType(prefix string, val reflect.Value, result map[string]interface{}) {
-	prefix = strings.TrimLeft(prefix, ".") // trim leading dot
-	valType := val.Type()
+	flattened["aws.ecs.container.arn"] = m.ContainerARN
+	flattened["aws.ecs.task.known.status"] = m.KnownStatus
+	flattened["aws.ecs.cluster"] = m.Labels.ComAmazonawsEcsCluster
+	flattened["aws.ecs.container.name"] = m.Labels.ComAmazonawsEcsContainerName
+	flattened["aws.ecs.task.arn"] = m.Labels.ComAmazonawsEcsTaskArn
+	flattened["aws.ecs.task.definition.family"] = m.Labels.ComAmazonawsEcsTaskDefinitionFamily
+	flattened["aws.ecs.task.definition.version"] = m.Labels.ComAmazonawsEcsTaskDefinitionVersion
+	flattened["created.at"] = m.CreatedAt.Format(time.RFC3339Nano)
+	flattened["desired.status"] = m.DesiredStatus
+	flattened["docker.id"] = m.DockerID
+	flattened["docker.name"] = m.DockerName
+	flattened["image"] = m.Image
+	flattened["image.id"] = m.ImageID
+	flattened["limits.cpu"] = m.Limits.CPU
+	flattened["limits.memory"] = m.Limits.Memory
+	flattened["name"] = m.Name
+	flattened["started.at"] = m.StartedAt.Format(time.RFC3339Nano)
+	flattened["type"] = m.Type
 
-	for i := 0; i < val.NumField(); i++ {
-		fieldType := valType.Field(i)
-		fieldValue := val.Field(i)
-
-		if fieldValue.Kind() == reflect.Ptr {
-			fieldValue = fieldValue.Elem()
-			if !fieldValue.IsValid() {
-				continue
-			}
-		}
-		flatTag, hasTag := fieldType.Tag.Lookup("flat")
-
-		switch fieldValue.Kind() {
-		case reflect.Struct:
-			flattenType(
-				prefix+flatTag+".",
-				fieldValue, result)
-
-		case reflect.Slice:
-			for j := 0; j < fieldValue.Len(); j++ {
-				sliceVal := fieldValue.Index(j)
-
-				if sliceVal.Kind() == reflect.String {
-					if hasTag {
-						result[fmt.Sprintf("%s%s.%d", prefix, flatTag, j)] = sliceVal.Interface()
-						continue
-					}
-				}
-
-				flattenType(fmt.Sprintf("%s%s.%d.", prefix, flatTag, j), sliceVal, result)
-			}
-		default:
-			if hasTag {
-				result[fmt.Sprintf("%s%s", prefix, flatTag)] = fieldValue.Interface()
-				continue
-			}
+	// add networks
+	for i, network := range m.Networks {
+		flattened[fmt.Sprintf("networks.%d.network.mode", i)] = network.NetworkMode
+		for ind, ipv4 := range network.IPv4Addresses {
+			flattened[fmt.Sprintf("networks.%d.ipv4.addresses.%d", i, ind)] = ipv4
 		}
 	}
+
+	// add ports
+	for i, port := range m.Ports {
+		flattened[fmt.Sprintf("ports.%d.container.port", i)] = port.ContainerPort
+		flattened[fmt.Sprintf("ports.%d.host.ip", i)] = port.HostIP
+		flattened[fmt.Sprintf("ports.%d.host.port", i)] = port.HostPort
+		flattened[fmt.Sprintf("ports.%d.protocol", i)] = port.Protocol
+	}
+
+	// add volumes
+	for i, volume := range m.Volumes {
+		flattened[fmt.Sprintf("volumes.%d.destination", i)] = volume.Destination
+		flattened[fmt.Sprintf("volumes.%d.source", i)] = volume.Source
+	}
+
+	return flattened
 }
