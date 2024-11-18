8016d4b866664ec0ce42f3163e31cb9d51ebf6ef
Ryan Tan <ryantanjunming@gmail.com>
2024-11-18 12:13:17 +0800

    try to set docker hostendpoints via envflags into the otel configmap

--- otel-integration/k8s-helm/e2e-test/main_test.go
+++ otel-integration/k8s-helm/e2e-test/main_test.go
@@ -69,8 +69,7 @@ func TestE2E_Agent(t *testing.T) {
 		k8stest.WaitForTelemetryGenToStart(t, k8sClient, info.Namespace, info.PodLabelSelectors, info.Workload, info.DataType)
 	}
 
-	WaitForMetrics(t, 5, metricsConsumer)
-	WaitForTraces(t, 5, tracesConsumer)
+	WaitForData(t, 5, metricsConsumer, tracesConsumer)
 
 	checkResourceMetrics(t, metricsConsumer.AllMetrics())
 	// checkGeneratedTraces(t, tracesConsumer.AllTraces())
--- otel-integration/k8s-helm/e2e-test/sinks.go
+++ otel-integration/k8s-helm/e2e-test/sinks.go
@@ -2,7 +2,6 @@ package e2e
 
 import (
 	"context"
-	"strconv"
 	"testing"
 	"time"
 
@@ -13,26 +12,6 @@ import (
 	"go.opentelemetry.io/collector/receiver/otlpreceiver"
 	"go.opentelemetry.io/collector/receiver/receivertest"
 )
-
-type MetricSinkConfig struct {
-	Ports    *ReceiverPorts
-	Consumer *consumertest.MetricsSink
-}
-
-type TraceSinkConfig struct {
-	Ports    *ReceiverPorts
-	Consumer *consumertest.TracesSink
-}
-
-type LogSinkConfig struct {
-	Ports    *ReceiverPorts
-	Consumer *consumertest.LogsSink
-}
-
-type ReceiverPorts struct {
-	Grpc int
-	Http int
-}
 
 func StartUpSinks(t *testing.T, mc *consumertest.MetricsSink, tc *consumertest.TracesSink) func() {
 	f := otlpreceiver.NewFactory()
@@ -50,34 +29,14 @@ func StartUpSinks(t *testing.T, mc *consumertest.MetricsSink, tc *consumertest.T
 	}
 }
 
-func setupReceiverPorts(cfg *otlpreceiver.Config, ports *ReceiverPorts) {
-	if ports != nil {
-		cfg.GRPC.NetAddr.Endpoint = "0.0.0.0:" + strconv.Itoa(ports.Grpc)
-		cfg.HTTP.Endpoint = "0.0.0.0:" + strconv.Itoa(ports.Http)
-	} else {
-		cfg.GRPC.NetAddr.Endpoint = "0.0.0.0:4317"
-		cfg.HTTP.Endpoint = "0.0.0.0:4318"
-	}
-}
-
-func WaitForMetrics(t *testing.T, entriesNum int, mc *consumertest.MetricsSink) {
-	timeoutSeconds := 180 // 3 minutes
+func WaitForData(t *testing.T, entriesNum int, mc *consumertest.MetricsSink, tc *consumertest.TracesSink) {
+	timeoutMinutes := 3
 	require.Eventuallyf(t, func() bool {
-		count := len(mc.AllMetrics())
-		t.Logf("Waiting for metrics: got %d/%d", count, entriesNum)
-		return count >= entriesNum
-	}, time.Duration(timeoutSeconds)*time.Second, 1*time.Second,
-		"failed to receive %d entries, received %d metrics in %d seconds", entriesNum,
-		len(mc.AllMetrics()), timeoutSeconds)
-}
-
-func WaitForTraces(t *testing.T, entriesNum int, tc *consumertest.TracesSink) {
-	timeoutSeconds := 180 // 3 minutes
-	require.Eventuallyf(t, func() bool {
-		count := len(tc.AllTraces())
-		t.Logf("Waiting for traces: got %d/%d", count, entriesNum)
-		return count >= entriesNum // Changed > to >=
-	}, time.Duration(timeoutSeconds)*time.Second, 1*time.Second,
-		"failed to receive %d entries, received %d traces in %d seconds", entriesNum,
-		len(tc.AllTraces()), timeoutSeconds)
+		// `telemetrygen` doesn't support profiles
+		// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/36127
+		// TODO: assert `len(pc.AllProfiles()) > entriesNum` once #36127 is resolved
+		return len(mc.AllMetrics()) > entriesNum && len(tc.AllTraces()) > entriesNum
+	}, time.Duration(timeoutMinutes)*time.Minute, 1*time.Second,
+		"failed to receive %d entries,  received %d metrics, %d traces in %d minutes", entriesNum,
+		len(mc.AllMetrics()), len(tc.AllTraces()), timeoutMinutes)
 }
