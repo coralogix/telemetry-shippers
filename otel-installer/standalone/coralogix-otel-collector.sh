a8afc3fd5288b50a7a18ac88cb172c8c7e91a799
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2026-08-25 11:20:17 +0200

    [otel-installer] Use 30s as config apply timeout for Supervisor
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- otel-installer/standalone/coralogix-otel-collector.sh
+++ otel-installer/standalone/coralogix-otel-collector.sh
@@ -1441,6 +1441,7 @@ capabilities:
 agent:
   executable: /usr/local/bin/otelcol-contrib
   passthrough_logs: true
+  config_apply_timeout: 30s
   description:
     non_identifying_attributes:
       service.name: "opentelemetry-collector"
